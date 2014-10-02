62b1f3e0f88432cd8b2f9c18fce810a696f88478
Yosuke Hara <yousuke.hara@gmail.com>
2014-10-02 14:05:17 +0900

    Modify script of making docs

--- make_rst_doc.sh
+++ make_rst_doc.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
-rm -rf doc/rst && mkdir doc/rst
 make doc
-pandoc --read=html --write=rst doc/leo_rpc.html -o doc/rst/leo_rpc.rst
-pandoc --read=html --write=rst doc/leo_rpc_client_conn.html -o doc/rst/leo_rpc_client_conn.rst
-pandoc --read=html --write=rst doc/leo_rpc_client_manager.html -o doc/rst/leo_rpc_client_manager.rst
-pandoc --read=html --write=rst doc/leo_rpc_client_utils.html -o doc/rst/leo_rpc_client_utils.rst
-pandoc --read=html --write=rst doc/leo_rpc_protocol.html -o doc/rst/leo_rpc_protocol.rst
-pandoc --read=html --write=rst doc/leo_rpc_server.html -o doc/rst/leo_rpc_server.rst
-pandoc --read=html --write=rst doc/leo_rpc_server_listener.html -o doc/rst/leo_rpc_server_listener.rst
+rm -rf doc/rst && mkdir doc/rst
+
+for Mod in leo_rpc \
+           leo_rpc_client_conn \
+           leo_rpc_client_manager \
+           leo_rpc_client_utils \
+           leo_rpc_protocol \
+           leo_rpc_server \
+           leo_rpc_server_listener
+do
+    read_file="doc/$Mod.html"
+    write_file="doc/rst/$Mod.rst"
+
+    pandoc --read=html --write=rst "$read_file" -o "$write_file"
+
+    sed -ie "1,6d" "$write_file"
+    sed -ie "s/\Module //" "$write_file"
+    LINE_1=`cat $write_file | wc -l`
+    LINE_2=`expr $LINE_1 - 10`
+    sed -ie "$LINE_2,\$d" "$write_file"
+done
+rm -rf doc/rst/*.rste
